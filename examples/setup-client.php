70574584a2a7d42ce73cc5e6a477ce592047e3a1
Felix Kiss <felix@felixkiss.com>
2016-01-02 21:11:26 +0100

    Only create new API client if there is none already

--- examples/setup-client.php
+++ examples/setup-client.php
@@ -4,17 +4,19 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Ultimed\ApiClient;
 
-$client =  new ApiClient([
-    'base_uri' => 'http://api.ultimed.app',
-    'client_id' => 2,
-    'client_secret' => 'foo',
-    // 'http_errors' => false, // Throw an exception for status codes {4xx,5xx}
-]);
-
-if (!$client->isAuthenticated() && empty($shouldNotAuthenticate)) {
-    // Login
-    $accessToken = require __DIR__ . '/authenticate.php';
-    $client->setAccessToken($accessToken);
+if (!isset($_client)) {
+    $_client = new ApiClient([
+        'base_uri' => 'http://api.ultimed.app',
+        'client_id' => 2,
+        'client_secret' => 'foo',
+        // 'http_errors' => false, // Throw an exception for status codes {4xx,5xx}
+    ]);
 }
 
-return $client;
+if (!$_client->isAuthenticated() && empty($shouldNotAuthenticate)) {
+    // Login
+    $accessToken = require __DIR__ . '/authenticate.php';
+    $_client->setAccessToken($accessToken);
+}
+
+return $_client;
