91df2fae0fbbbb2b0c4d03644489265b864bd330
Michael Pitzer <mail@mpitzer.at>
2020-10-12 03:01:40 +0200

    Support `date` in file update

--- src/Ultimed/Requests/FileUpdate.php
+++ src/Ultimed/Requests/FileUpdate.php
@@ -1,11 +1,16 @@
 <?php namespace Ultimed\Requests;
 
+use Ultimed\Presenters\EmberDate;
+
 class FileUpdate extends ApiRequest
 {
     use IncludesOAuthAccessToken;
 
     public function __construct($id, array $data = [])
     {
+        if (array_key_exists('date', $data)) {
+            $data['date'] = (new EmberDate($data['date']))->format();
+        }
         $body = json_encode(['file' => $data]);
         parent::__construct('PUT', "v1/files/$id", [], $body);
     }
